bbe20bb0480a1ac46bbf14816f389cf01cad884f
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-04-26 09:50:14 +0300

    Update

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int levelCount = 4;
+            Stack<int> tower1 = new(), tower2 = new(), tower3 = new();
+
+            for (int i = levelCount * 2 - 1; i > 0; i -= 2)
+                tower1.Push(i);
+
+
         }
     }
 }
--- Task5/Tower.cs
+++ Task5/Tower.cs
@@ -8,5 +8,6 @@ namespace Task5
 {
     internal class Tower
     {
+        
     }
 }
